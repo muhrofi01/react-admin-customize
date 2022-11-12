6ab4a57f15790482604689641bf335e5e7091bb7
Dipterocarpaceae <bagusalmahenzar@gmail.com>
2022-11-12 22:31:30 +0700

    Dialog login dan if login dialog close

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import * as React from "react";
-import { Admin, Resource, CustomRoutes, useAuthState, Loading } from 'react-admin';
-import { Route } from 'react-router-dom';
-import jsonServerProvider from 'ra-data-json-server';
-import { UserList } from './users';
-import MyLayout from './MyLayout';
+import { Admin, Resource, CustomRoutes } from "react-admin";
+import { Route } from "react-router-dom";
+import jsonServerProvider from "ra-data-json-server";
+import { UserList } from "./users";
+import MyLayout from "./MyLayout";
 import authProvider from "./authProvider";
 import { PostList } from "./posts";
 import Home from "./Home";
 import RedirectLogin from "./Redirect";
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const checkLogin = () => {
+  return localStorage.getItem("username") ? false : true;
+};
 
 const App = () => {
   return (
     <Admin dataProvider={dataProvider} layout={MyLayout} loginPage={<RedirectLogin />} authProvider={authProvider}>
       <CustomRoutes>
-        <Route path="/" element={<Home openLogin={"true"} />} />
+        <Route path="/" element={<Home openLogin={checkLogin} />} />
       </CustomRoutes>
       <Resource name="users" list={UserList} />
       <Resource name="posts" list={PostList} />
     </Admin>
-  )
+  );
 };
 
 export default App;
