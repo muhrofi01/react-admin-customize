1ef050a9ac833117041fb0934ffd122f3084e6a9
rii92 <riofebri16@gmail.com>
2022-11-13 10:50:33 +0700

    menu login sudah sesuai kondisi

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,38 +1,34 @@
-import * as React from 'react';
-import { AppBar, Logout, Menu, UserMenu, Login} from 'react-admin';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import SettingsIcon from '@mui/icons-material/Settings';
-import LoginIcon from '@mui/icons-material/Login';
+import LoginIcon from "@mui/icons-material/Login";
+import * as React from "react";
+import { AppBar, Logout, Menu, UserMenu } from "react-admin";
 
+const checkLogin = () => {
+  return localStorage.getItem("username") ? true : false;
+};
+
+console.log(checkLogin());
 
 // It's important to pass the ref to allow MUI to manage the keyboard navigation
-const ConfigurationMenu = () => {
-    return (
-        <MenuItem>
-            <ListItemIcon>
-                <SettingsIcon />
-            </ListItemIcon>
-            <ListItemText>
-               Configuration
-            </ListItemText>
-        </MenuItem>
-    );
-};
-
 const LoginMenu = () => {
-    return ( 
-            <Menu.Item sx={{ color: 'black' }} to="/login" primaryText="Login" leftIcon={<LoginIcon/>} />
+  if (checkLogin() == true) {
+    return <Logout sx={{ color: "black" }} />;
+  } else {
+    return (
+      <Menu.Item
+        sx={{ color: "black" }}
+        to="/login"
+        primaryText="Login"
+        leftIcon={<LoginIcon />}
+        disabled={checkLogin()}
+      />
     );
+  }
 };
 
-const MyUserMenu = props => (
-    <UserMenu {...props}>
-        <ConfigurationMenu />
-        <Logout />
-        <LoginMenu />
-    </UserMenu>
+const MyUserMenu = (props) => (
+  <UserMenu {...props}>
+    <LoginMenu />
+  </UserMenu>
 );
 
 const MyAppBar = (props) => <AppBar {...props} userMenu={<MyUserMenu />} />;
