9fb4e7caecb39e6475fda0d67f72f1d87322daa9
rii92 <riofebri16@gmail.com>
2022-11-17 17:38:50 +0700

    appbar

--- src/AppBar.js
+++ src/AppBar.js
@@ -2,32 +2,22 @@ import LoginIcon from "@mui/icons-material/Login";
 import * as React from "react";
 import { AppBar, Logout, Menu, UserMenu } from "react-admin";
 
-const checkLogin = () => {
-  return localStorage.getItem("username") ? true : false;
-};
-
-console.log(checkLogin());
-
 // It's important to pass the ref to allow MUI to manage the keyboard navigation
 const LoginMenu = () => {
-  if (checkLogin() == true) {
-    return <Logout sx={{ color: "black" }} />;
-  } else {
-    return (
-      <Menu.Item
-        sx={{ color: "black" }}
-        to="/login"
-        primaryText="Login"
-        leftIcon={<LoginIcon />}
-        disabled={checkLogin()}
-      />
-    );
-  }
+  return (
+    <Menu.Item
+      sx={{ color: "black" }}
+      to="/login"
+      primaryText="Login"
+      leftIcon={<LoginIcon />}
+    />
+  );
 };
 
 const MyUserMenu = (props) => (
   <UserMenu {...props}>
     <LoginMenu />
+    <Logout sx={{ color: "black" }} />
   </UserMenu>
 );
 
